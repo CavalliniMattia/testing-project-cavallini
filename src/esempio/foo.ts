d8a43316fd256f34c508bcd6a11d3046aa55a61a
CavalliniMattia <mattia.cavallini18@gmail.com>
2024-02-09 12:48:05 +0100

    implementazione funzione addGiftcard

--- src/esempio/foo.ts
+++ src/esempio/foo.ts
@@ -26,3 +26,57 @@ export const newOrder = (customerInfo: {
 
   return order;
 };
+
+export const addGiftcard = (
+  order: {
+    orderId: number;
+    customer: {
+      codiceFiscale: string;
+      nome: string;
+      cognome: string;
+      email: string;
+    };
+    giftcards: {
+      tipologia: "digitale" | "cartacea";
+      taglio: 10 | 20 | 50 | 100;
+      quantità: number;
+    }[];
+  },
+  giftcard: {
+    tipologia: string;
+    taglio: number;
+    quantità: number;
+  }
+): {
+  orderId: number;
+  customer: {
+    codiceFiscale: string;
+    nome: string;
+    cognome: string;
+    email: string;
+  };
+  giftcards: {
+    tipologia: "digitale" | "cartacea";
+    taglio: 10 | 20 | 50 | 100;
+    quantità: number;
+  }[];
+} => {
+  if (
+    (giftcard.tipologia !== "digitale" && giftcard.tipologia !== "cartacea") ||
+    ![10, 20, 50, 100].includes(giftcard.taglio)
+  ) {
+    throw new Error("Tipologia o taglio della giftcard non validi");
+  }
+
+  if (!order.giftcards) {
+    order.giftcards = [];
+  }
+
+  order.giftcards.push({
+    tipologia: giftcard.tipologia as "digitale" | "cartacea",
+    taglio: giftcard.taglio as 10 | 20 | 50 | 100,
+    quantità: giftcard.quantità,
+  });
+
+  return order;
+};
