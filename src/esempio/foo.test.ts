cf4c7ed442ce511db2f16f1b013193787cc28a31
CavalliniMattia <mattia.cavallini18@gmail.com>
2024-02-09 11:45:57 +0100

    test falliti newOrder

--- src/esempio/foo.test.ts
+++ src/esempio/foo.test.ts
@@ -1,9 +1,84 @@
-import { sum } from './foo';
+import { newOrder } from "./foo";
+describe("Test in classe", () => {
+  test("Test generazione orderId", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
 
-test('basic', () => {
-  expect(sum()).toBe(0);
-});
+    const order = newOrder(customerInfo);
 
-test('basic again', () => {
-  expect(sum(1, 2)).toBe(3);
+    expect(order).toHaveProperty("orderId");
+  });
+
+  test("Test correttezza informazioni cliente", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
+
+    const order = newOrder(customerInfo);
+
+    expect(order.customer).toEqual(customerInfo);
+  });
+
+  test("Test definizione campi cliente", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
+
+    const order = newOrder(customerInfo);
+
+    expect(order.customer).toHaveProperty("codiceFiscale");
+    expect(order.customer).toHaveProperty("nome");
+    expect(order.customer).toHaveProperty("cognome");
+    expect(order.customer).toHaveProperty("email");
+  });
+
+  test("Test lunghezza codice fiscale", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
+
+    const order = newOrder(customerInfo);
+
+    expect(order.customer.codiceFiscale.length).toBe(16);
+  });
+
+  test("Test formato codice fiscale", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
+
+    const order = newOrder(customerInfo);
+
+    expect(order.customer.codiceFiscale).toMatch(/^[a-zA-Z0-9]+$/);
+  });
+
+  test("Test validità nome e cognome", () => {
+    const customerInfo = {
+      codiceFiscale: "RSSMRA85T10A562S",
+      nome: "Mario",
+      cognome: "Rossi",
+      email: "mario@example.com",
+    };
+
+    const order = newOrder(customerInfo);
+
+    expect(order.customer.nome).toMatch(/^[a-zA-Z]+$/);
+    expect(order.customer.cognome).toMatch(/^[a-zA-Z]+$/);
+  });
 });
