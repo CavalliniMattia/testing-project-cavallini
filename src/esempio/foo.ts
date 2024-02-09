37a9bf3cf787403a3a38f706f84eec24015b1087
CavalliniMattia <mattia.cavallini18@gmail.com>
2024-02-09 11:54:13 +0100

    implementazione funzione newOrder

--- src/esempio/foo.ts
+++ src/esempio/foo.ts
@@ -1,1 +1,27 @@
+export const newOrder = (customerInfo: {
+  codiceFiscale: string;
+  nome: string;
+  cognome: string;
+  email: string;
+}): {
+  orderId: number;
+  customer: {
+    codiceFiscale: string;
+    nome: string;
+    cognome: string;
+    email: string;
+  };
+} => {
+  const orderId = Math.floor(Math.random() * 1000) + 1;
 
+  const order = {
+    orderId: orderId,
+    customer: {
+      codiceFiscale: customerInfo.codiceFiscale,
+      nome: customerInfo.nome,
+      cognome: customerInfo.cognome,
+      email: customerInfo.email,
+    },
+  };
+  return order;
+};
